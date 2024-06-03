4eb9cb5d0d0322bff237a29d85a104c185fb33ac
soHyn <sohyn2208@gmail.com>
2024-06-04 01:55:41 +0900

    refactor: 작품 조회 API 수정

--- src/main/java/geulsam/archive/domain/content/dto/res/ContentRes.java
+++ src/main/java/geulsam/archive/domain/content/dto/res/ContentRes.java
@@ -1,29 +1,31 @@
 package geulsam.archive.domain.content.dto.res;
 
-
 import com.fasterxml.jackson.annotation.JsonFormat;
+import geulsam.archive.domain.content.entity.Genre;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.time.LocalDate;
-import java.time.Year;
+import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
 public class ContentRes {
     @Schema(example = "0")
     private int id;
     @Schema(example = "adsa-dsafdsa-vsadf-dsafdsa")
-    private String contentId;
+    private String contentId;  //
     @Schema(example = "소설")
-    private String type;
+    private Genre type;
     @Schema(example = "때때로 나는 회색분자라는 소리를 듣는다")
     private String title;
     @Schema(example = "2024")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy년")
-    private Year year;
+    private LocalDate year;
     @Schema(example = "하수민")
     private String author;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy년 M월 d일")
     @Schema(example = "2024년3월10일")
-    private LocalDate createdAt;
+    private LocalDateTime createdAt;
 }
--- src/main/java/geulsam/archive/domain/content/service/ContentService.java
+++ src/main/java/geulsam/archive/domain/content/service/ContentService.java
@@ -3,22 +3,37 @@ package geulsam.archive.domain.content.service;
 import geulsam.archive.domain.content.dto.res.ContentRes;
 import geulsam.archive.domain.content.entity.Content;
 import geulsam.archive.domain.content.repository.ContentRepository;
+import geulsam.archive.domain.contentAward.entity.ContentAward;
+import geulsam.archive.domain.contentAward.repository.ContentAwardRepository;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Service
 public class ContentService {
 
     private ContentRepository contentRepository;
+    private ContentAwardRepository contentAwardRepository;
 
     public List<ContentRes> getContents(Pageable pageable) {
-        List<Content> contents = contentRepository.findAll(pageable).getContent();
-        List<ContentRes> contentResList = new ArrayList<>();
-
-        return contentResList;   //
+        List<Content> findContents = contentRepository.findAll(pageable).getContent();
+        return IntStream.range(0, findContents.size())
+                .mapToObj(i -> {
+                    Content content = findContents.get(i);
+                    int contentId = content.getId();
+                    ContentAward contentAward = contentAwardRepository.getReferenceById(contentId);
+                    return new ContentRes(
+                            contentId,
+                            content.getUrl(),
+                            content.getGenre(),
+                            content.getName(),
+                            contentAward.getContentAwardAt(),   //
+                            content.getUser().getName(),
+                            content.getCreatedAt()
+                    );
+                }).collect(Collectors.toList());
     }
-
 }
